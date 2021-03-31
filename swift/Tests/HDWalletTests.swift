bee06de140cc94639b4962ae9a36aae4e31dd55b
Eric li <weilong@crypto.com>
2021-03-31 12:05:02 +0800

    🔧Update podfile and complier error

--- swift/Tests/HDWalletTests.swift
+++ swift/Tests/HDWalletTests.swift
@@ -421,14 +421,15 @@ class HDWalletTests: XCTestCase {
 
         XCTAssertEqual(address, "band1pe8xm2r46rmctsukuqu7gl900vzprfsp4sguc3")
     }
-    
+
     func testDeriveCro() {
         let coin = CoinType.cro
         let key = HDWallet.test.getKeyForCoin(coin: coin)
         let address = coin.deriveAddress(privateKey: key)
         XCTAssertEqual(address, "cro1qhw03a43g23m9877czt6a0n0cmqyel6hjnqjy4")
     }
-     func testDeriveCro() {
+
+    func testDerivetCro() {
         let coin = CoinType.tcro
         let key = HDWallet.test.getKeyForCoin(coin: coin)
         let address = coin.deriveAddress(privateKey: key)
